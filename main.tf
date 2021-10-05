a2f3560da1486bbb86d1ac1af7eaff3a96b1a7aa
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-10-05 13:33:51 +0530

    ss

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ terraform {
     name = "terraform_prac"
     }
   }
+  required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 3.27"
+    }
+ }
+
+  required_version = ">= 0.14.9"
+}
 
 }
 provider "aws" {
