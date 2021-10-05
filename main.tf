d9d6fc90df75eef90df1dc6134baa28aa4ffae18
PradeepreddyKapa <pradeepreddy@zelarsoft.com>
2021-10-05 12:50:42 +0530

    ss

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+  backend "remote" {
+    organization = "pradeep349"
+  workspaces {
+    name = "terraform_prac"
     }
   }
 
-  required_version = ">= 0.14.9"
 }
-
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
--- main1.tf
+++ main1.tf
@@ -1,26 +1,26 @@
-terraform {
-  backend "remote" {
-    organization = "pradeep349"
-    workspaces {
-      name = "terraform_prac"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
+# terraform {
+#   backend "remote" {
+#     organization = "pradeep349"
+#     workspaces {
+#       name = "terraform_prac"
+#     }
+#   }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.27"
+#     }
+#   }
 
-  required_version = ">= 0.14.9"
-}
+#   required_version = ">= 0.14.9"
+# }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
+# provider "aws" {
+#   profile = "default"
+#   region  = "us-west-2"
+# }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-08d70e59c07c61a3a"
+#   instance_type = "t2.micro"
+# }
